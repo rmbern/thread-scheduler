49ada710677fe311d1788e9a59469f0edf773361
ryan <rmb280@scarletmail.rutgers.edu>
2018-04-06 14:10:13 -0400

    dll c and h files added (forgot them in last commit...oops)

---
+++ dll.h
@@ -0,0 +1,20 @@
+typedef enum direction {NEXT, PREV} direction;
+
+typedef struct dll
+{	
+	struct dll_node * head;
+	int is_consistent;
+} dll;
+
+typedef struct dll_node
+{
+	struct dll_node * next;
+	struct dll_node * prev;
+	int data;
+
+} dll_node;
+
+dll * dll_init(int data);
+void dll_add_node(dll * list, int data);
+void dll_remove_node(dll * list, dll_node * node);
+void dll_print(dll * list, int hops, direction dir);
